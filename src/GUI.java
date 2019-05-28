fcc9d8707237b648ab65c882bc3aeead8d681f50
Mikaela <mikaelaysanchez@gmail.com>
2019-05-28 15:44:37 +0100

    Add a text field and button component

--- src/GUI.java
+++ src/GUI.java
@@ -15,7 +15,23 @@ public class GUI extends JFrame {
         setSize(400, 500);
         setLocation(500, 200);
 
-        // Implement calculator components
+        // Instantiate calculator components
+        TextField textField = new TextField();
+        Button calculateBtn = new Button("Calculate");
+        JPanel panel2 = new JPanel();
+
+        // Set calculate button attributes
+        calculateBtn.setSize(60,60);
+        calculateBtn.setLocation(20,90);
+
+        // Set text field attributes
+        textField.setSize(250,40);
+        textField.setLocation(20,20);
+
+        // Add components to calculator window
+        add(textField);
+        add(calculateBtn);
+        add(panel2);
 
 
         // Make calculator visible
